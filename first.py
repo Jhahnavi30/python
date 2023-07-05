fd3b855d46167a7ac299fae5d8d1e1b4db284814
Jhahnavi30 <138557454+Jhahnavi30@users.noreply.github.com>
2023-07-05 22:18:33 +0530

    changes made

--- first.py
+++ first.py
@@ -28,3 +28,46 @@ power = 5
 result = exponentiate(base, power)
 print(result)
 
+
+#Python function that returns a dictionary
+def myAnimals(a1,a2,a3):
+    Animalgroup = {'Kitten':a1,'Puppy':a2,'Pup':a3}
+    return Animalgroup
+
+output = myAnimals("Cat","Dog","Rat")
+print(output)
+
+#classes
+class student:
+    college_name = "Reva University"
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def info(self):
+        marks = [1, 2, 3, 4, 5]
+        self.college()
+        print(f"Name: {self.name} | Age: {self.age} | Gender: {self.gender}")
+        self.total_marks(marks)
+
+    @classmethod
+    def college(cls):
+        print(f"College: {cls.college_name}")
+
+    @staticmethod
+    def total_marks(marks_list):
+        total = 0
+        for mark in marks_list:
+            total = total + mark
+        print(f"Total marks = {total}")
+        return total
+
+
+# Create an instance of the student class
+student1 = student("John", 20, "Male")
+
+# Call the info method to display student information
+student1.info()
+
